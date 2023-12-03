eb4f312f6b0f12b49c9dd45361d0933c5547ee83
aalis1 <aalisva1@gmail.com>
2023-12-03 17:48:51 +0300

    onClick repair

--- h.w.2.3.2.js
+++ h.w.2.3.2.js
@@ -1,20 +1,3 @@
-// class Button {
-//   constructor(type, color, width, height) {
-//     this.type = type;
-//     this.color = color;
-//     this.width = width;
-//     this.height = height;
-//   }
-//   onClick() {
-//     console.log(type, color, width, height);
-//   }
-// }
-// let greenButton = new Button("button", "green", 15, 10);
-// console.log(greenButton.onClick());
-
-// let testButton = new Button();
-// if ((testButton = greenButton)) console.log(testButton instanceof Button);
-
 class Button {
   constructor(type, color, width, height) {
     this.type = type;
@@ -26,14 +9,31 @@ class Button {
     console.log(this.type, this.color, this.width, this.height);
   }
 }
-let greenButton = new Button("submit", "green", 15, 10);
-greenButton.onClick();
+let greenButton = new Button("button", "green", 15, 10);
+console.log(greenButton.onClick());
 
-function testButton(someButton) {
-  if (someButton instanceof Button) {
-    console.log("greenButton is instance of Button");
-  } else {
-    console.log("something went wrong");
-  }
-}
-testButton(greenButton);
+let testButton = new Button();
+if ((testButton = greenButton)) console.log(testButton instanceof Button);
+
+// class Button {
+//   constructor(type, color, width, height) {
+//     this.type = type;
+//     this.color = color;
+//     this.width = width;
+//     this.height = height;
+//   }
+//   onClick() {
+//     console.log(this.type, this.color, this.width, this.height);
+//   }
+// }
+// let greenButton = new Button("submit", "green", 15, 10);
+// greenButton.onClick();
+
+// function testButton(someButton) {
+//   if (someButton instanceof Button) {
+//     console.log("greenButton is instance of Button");
+//   } else {
+//     console.log("something went wrong");
+//   }
+// }
+// testButton(greenButton);
