8552a9ce35c0fc648bd964972958b42c18c9fe10
aalis1 <aalisva1@gmail.com>
2023-12-03 13:01:39 +0300

    onClick dont work

--- h.w.2.3.2.js
+++ h.w.2.3.2.js
@@ -1,3 +1,20 @@
+// class Button {
+//   constructor(type, color, width, height) {
+//     this.type = type;
+//     this.color = color;
+//     this.width = width;
+//     this.height = height;
+//   }
+//   onClick() {
+//     console.log(type, color, width, height);
+//   }
+// }
+// let greenButton = new Button("button", "green", 15, 10);
+// console.log(greenButton);
+
+// let testButton = new Button();
+// if ((testButton = greenButton)) console.log(testButton instanceof Button);
+
 class Button {
   constructor(type, color, width, height) {
     this.type = type;
@@ -9,8 +26,14 @@ class Button {
     console.log(type, color, width, height);
   }
 }
-let greenButton = new Button("button", "green", 15, 10);
-console.log(greenButton);
+let greenButton = new Button("submit", "green", 30, 40);
+console.log(greenButton.onClick());
 
-let testButton = new Button();
-if ((testButton = greenButton)) console.log(testButton instanceof Button);
+function testButton(someButton) {
+  if (someButton instanceof Button) {
+    console.log("greenButton is instance of Button");
+  } else {
+    console.log();
+  }
+}
+testButton(greenButton);
